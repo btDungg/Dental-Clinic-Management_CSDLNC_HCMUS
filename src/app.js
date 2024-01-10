97b140e8e457ea0223b838fbed215fe01d30c6be
21120434 <dungtb1402@gmail.com>
2024-01-10 13:38:05 +0700

    home shedule

--- src/app.js
+++ src/app.js
@@ -17,11 +17,39 @@ app.set("views", path.join(__dirname, "views"));
 // set static files
 app.use(express.static("public"));
 
-app.get("/home", (req, res) => {
+app.get("/admin/home", (req, res) => {
     res.render("home", {
+        isAdmin:true,
         title: "Home Page",
     });
 });
+
+app.get("/staff/home", (req, res) => {
+    res.render("home", {
+        isStaff:true,
+        title: "Home Page",
+    });
+});
+
+app.get("/staff/shedule", (req, res) => {
+    res.render("shedule", {
+        isStaff:true,
+        title: "Shedule Page",
+    });
+});
+app.get("/add_shedule", (req, res) => {
+    res.render("add_shedule", {
+        title: "Add Shedule Page",
+    });
+});
+
+app.get("/admin/shedule", (req, res) => {
+    res.render("shedule", {
+        isAdmin:true,
+        title: "Shedule Page",
+    });
+});
+
     app.get("/login", (req, res) => {
         res.render("login", {
             title: "Login page",
