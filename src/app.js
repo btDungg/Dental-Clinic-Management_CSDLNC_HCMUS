b672910db0d849e719bfd577a024346ae7099855
21120434 <dungtb1402@gmail.com>
2024-01-10 16:17:54 +0700

    profile customer dentist staff

--- src/app.js
+++ src/app.js
@@ -19,21 +19,68 @@ app.use(express.static("public"));
 
 app.get("/admin/home", (req, res) => {
     res.render("home", {
-        isAdmin:true,
+        isAdmin: true,
         title: "Home Page",
     });
 });
 
 app.get("/staff/home", (req, res) => {
     res.render("home", {
-        isStaff:true,
+        isStaff: true,
         title: "Home Page",
     });
 });
 
+app.get("/dentist/home", (req, res) => {
+    res.render("home", {
+        isDentist: true,
+        title: "Home Page",
+    });
+});
+
+
+
+app.get("/dentist/list_dentist", (req, res) => {
+    res.render("manage_dentist", {
+        isDentist: true,
+        title: "Dentist Page",
+    });
+});
+
+app.get("/staff/list_dentist", (req, res) => {
+    res.render("manage_dentist", {
+        isStaff: true,
+        title: "Dentist Page",
+    });
+});
+app.get("/admin/list_dentist", (req, res) => {
+    res.render("manage_dentist", {
+        isAdmin: true,
+        title: "Dentist Page",
+    });
+});
+app.get("/admin/list_staff", (req, res) => {
+    res.render("manage_staff", {
+        isAdmin: true,
+        title: "Staff Page",
+    });
+});
+app.get("/dentist/list_staff", (req, res) => {
+    res.render("manage_staff", {
+        isDentist: true,
+        title: "Staff Page",
+    });
+}); app.get("/staff/list_staff", (req, res) => {
+    res.render("manage_staff", {
+        isStaff: true,
+        title: "Staff Page",
+    });
+});
+
+
 app.get("/staff/shedule", (req, res) => {
     res.render("shedule", {
-        isStaff:true,
+        isStaff: true,
         title: "Shedule Page",
     });
 });
@@ -45,16 +92,55 @@ app.get("/add_shedule", (req, res) => {
 
 app.get("/admin/shedule", (req, res) => {
     res.render("shedule", {
-        isAdmin:true,
+        isAdmin: true,
         title: "Shedule Page",
     });
 });
 
-    app.get("/login", (req, res) => {
-        res.render("login", {
-            title: "Login page",
+app.get("/admin/customer", (req, res) => {
+    res.render("customer", {
+        isAdmin:true,
+        title: "Customer Page",
+    });
+});
+app.get("/dentist/customer", (req, res) => {
+    res.render("customer", {
+        isDentist:true,
+        title: "Customer Page",
+    });
+    });
+app.get("/staff/customer", (req, res) => {
+        res.render("customer", {
+            isStaff:true,
+            title: "Customer Page",
         });
     });
+
+    app.get("/staff/profile", (req, res) => {
+        res.render("profile", {
+            isStaff:true,
+            title: "Profile Page",
+        });
+    });
+
+    app.get("/admin/profile", (req, res) => {
+        res.render("profile", {
+            isAdmin:true,
+            title: "Profile Page",
+        });
+    });
+    app.get("/dentist/profile", (req, res) => {
+        res.render("profile", {
+            isDentist:true,
+            title: "Profile Page",
+        });
+    });
+
+app.get("/login", (req, res) => {
+    res.render("login", {
+        title: "Login page",
+    });
+});
 app.get("/signup", (req, res) => {
     res.render("signup", {
         title: "Signup page",
